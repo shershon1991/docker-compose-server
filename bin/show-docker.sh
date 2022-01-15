f6058893cfed96d3569def07525039b62f777cfc
谭晓珊 <tanxiaoshan@weimiao.cn>
2022-01-15 10:32:55 +0800

    update

--- bin/show-docker.sh
+++ bin/show-docker.sh
@@ -1,1 +1,1 @@
-docker inspect --format='{{.Name}} - {{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $(docker ps -aq)
+docker inspect --format='{{.Name}} {{range .NetworkSettings.Networks}} {{.IPAddress}} {{end}}' $(docker ps -aq)
