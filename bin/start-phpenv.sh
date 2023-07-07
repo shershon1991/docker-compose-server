d921dd8386080912abcb7d5091fe4619b8285878
tanxiaoshan <18810877074@163.com>
2023-07-07 14:54:25 +0800

    update

--- bin/start-phpenv.sh
+++ bin/start-phpenv.sh
@@ -1,4 +1,4 @@
 docker-compose stop workspace nginx mysql php-fpm redis &&
-  #docker-compose rm -f workspace nginx mysql php-fpm redis &&
-  #docker-compose build workspace nginx mysql php-fpm redis &&
+  docker-compose rm -f workspace nginx mysql php-fpm redis &&
+  docker-compose build workspace nginx mysql php-fpm redis &&
   docker-compose up -d workspace nginx mysql php-fpm redis
