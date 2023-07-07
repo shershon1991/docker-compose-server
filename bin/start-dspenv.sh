246659724d5c0a6256843b40ad7f9ba4737ce9cd
tanxiaoshan <18810877074@163.com>
2023-07-07 15:00:51 +0800

    update

--- bin/start-dspenv.sh
+++ bin/start-dspenv.sh
@@ -1,2 +1,4 @@
 docker-compose stop dsp mysql redis rabbitmq &&
+  docker-compose rm -f dsp mysql redis rabbitmq &&
+  docker-compose build dsp mysql redis rabbitmq &&
   docker-compose up -d dsp mysql redis rabbitmq
